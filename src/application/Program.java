ecbf0b83b46db226bf19a7b40c364f76153f08f4
João Pedro <joaopedrov159@hotmail.com>
2024-05-24 09:31:13 -0300

    Class seller created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department dep = new Department(1, "books");
+		Seller sel = new Seller(21, "John", "john@gmail.com", new Date(), 3000.00, dep);
 		System.out.println(dep);
+		System.out.println(sel);
 	}
 }
