5cbaedcf079cea35cd0e9c469a908efc9bcba33a
João Pedro <joaopedrov159@hotmail.com>
2024-05-24 11:04:42 -0300

    DAO Factory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,37 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao{
+
+	@Override
+	public void create(Seller dep) {
+		
+	}
+
+	@Override
+	public void update(Seller dep) {
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		
+	}
+
+	@Override
+	public Seller read(Integer id) {
+		
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		
+		return null;
+	}
+	
+}
